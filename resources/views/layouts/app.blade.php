0d612e8bf305f9f838e2676ab54a7004477d03ee
tushar bansal <tuskibansal@gmail.com>
2017-10-02 23:16:14 +0530

    sign in signup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,41 +8,50 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        
+                <!-- bootstrap laravel -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap-responsive.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrappage.css') }}" rel="stylesheet">
+
+        
+        <!-- global styles laravel -->
+        <link href="{{ asset('css/flexslider.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+        
+
+        <!-- scripts laravel -->
+        <script src="{{ asset('js/jquery-1.7.2.min.js') }}" rel="stylesheet"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}" rel="stylesheet"></script>
+        <script src="{{ asset('js/superfish.js') }}" rel="stylesheet"></script>
+        <script src="{{ asset('js/jquery.scrolltotop.js') }}" rel="stylesheet"></script>
+        
+
+
+
+
+    <title>{{ config('app.name', 'tushar') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+<!--  -->
+            <div id="top-bar" class="container">
+            <div class="row">
+                <div class="span4">
+                    <form method="POST" class="search_form">
+                        <input type="text" class="input-block-level search-query" Placeholder="eg. T-sirt">
+                    </form>
                 </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
+                <div class="span8">
+                    <div class="account pull-right">
+                        <ul class="user-menu">              
+                            <li><a href="#">My Account</a></li>
+                            <li><a href="cart">Your Cart</a></li>
+                            <li><a href="checkout">Checkout</a></li>                    
+                            @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
@@ -65,15 +74,103 @@
                                     </li>
                                 </ul>
                             </li>
-                        @endguest
-                    </ul>
+                        @endguest      
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </div>
+            
+        <div id="wrapper" class="container">
+            <section class="navbar main-menu">
+                <div class="navbar-inner main-menu">                
+                    <a href="index.html" class="logo pull-left"><img src={{ asset('img/logo.png')}} src="themes/images//logo.png" class="site_logo" alt=""></a>
+                    <nav id="menu" class="pull-right">
+                        <ul>
+                            <li><a href="./products.html">Woman</a>                 
+                                <ul>
+                                    <li><a href="./products.html">Lacinia nibh</a></li>                                 
+                                    <li><a href="./products.html">Eget molestie</a></li>
+                                    <li><a href="./products.html">Varius purus</a></li>                                 
+                                </ul>
+                            </li>                                                           
+                            <li><a href="./products.html">Man</a></li>          
+                            <li><a href="./products.html">Sport</a>
+                                <ul>                                    
+                                    <li><a href="./products.html">Gifts and Tech</a></li>
+                                    <li><a href="./products.html">Ties and Hats</a></li>
+                                    <li><a href="./products.html">Cold Weather</a></li>
+                                </ul>
+                            </li>                           
+                            <li><a href="./products.html">Hangbag</a></li>
+                            <li><a href="./products.html">Best Seller</a></li>
+                            <li><a href="./products.html">Top Seller</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>          
+            </div>
+
 
         @yield('content')
     </div>
+    <div id="wrapper" class="container">
+    <section id="footer-bar">
+                <div class="row">
+                    <div class="span3">
+                        <h4>Navigation</h4>
+                        <ul class="nav">
+                            <li><a href="./index.html">Homepage</a></li>  
+                            <li><a href="./about.html">About Us</a></li>
+                            <li><a href="./contact">Contac Us</a></li>
+                            <li><a href="./cart.html">Your Cart</a></li>
+                            <li><a href="./register.html">Login</a></li>                            
+                        </ul>                   
+                    </div>
+                    <div class="span4">
+                        <h4>My Account</h4>
+                        <ul class="nav">
+                            <li><a href="#">My Account</a></li>
+                            <li><a href="#">Order History</a></li>
+                            <li><a href="#">Wish List</a></li>
+                            <li><a href="#">Newsletter</a></li>
+                        </ul>
+                    </div>
+                    <div class="span5">
+                        <p class="logo"><img src={{asset('img/logo.png')}} src="themes/images/logo.png" class="site_logo" alt=""></p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. the  Lorem Ipsum has been the industry's standard dummy text ever since the you.</p>
+                        <br/>
+                        <span class="social_icons">
+                            <a class="facebook" href="#">Facebook</a>
+                            <a class="twitter" href="#">Twitter</a>
+                            <a class="skype" href="#">Skype</a>
+                            <a class="vimeo" href="#">Vimeo</a>
+                        </span>
+                    </div>                  
+                </div>  
+            </section>
+            <section id="copyright">
+                <span>Copyright {{ date('Y') }} Shopping cart  All right reserved.</span>
+            </section>
+            </div>
 
+        </div>
+        <script src="themes/js/common.js"></script>
+        <script src="themes/js/jquery.flexslider-min.js"></script>
+        <script type="text/javascript">
+            $(function() {
+                $(document).ready(function() {
+                    $('.flexslider').flexslider({
+                        animation: "fade",
+                        slideshowSpeed: 4000,
+                        animationSpeed: 600,
+                        controlNav: false,
+                        directionNav: true,
+                        controlsContainer: ".flex-container" // the container that holds the flexslider
+                    });
+                });
+            });
+        </script>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
